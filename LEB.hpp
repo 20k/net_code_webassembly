0b97b6505ea0d694b2e0e6d905b926f156909360
James <Icedshot@gmail.com>
2018-11-20 02:06:08 +0000

    fixes, more tests

--- LEB.hpp
+++ LEB.hpp
@@ -2,20 +2,22 @@
 #define LEB_HPP_INCLUDED
 
 #include "types.hpp"
+#include <bitset>
 
 namespace leb
 {
     template<typename T>
+    inline
     T signed_decode(data& in)
     {
         T result = 0;
-        uint64_t shift = 0;
+        int64_t shift = 0;
         uint8_t byte = 0;
 
         while(true)
         {
             byte = in.next();
-            result |= (0x7F) << shift;
+            result |= (0x7F & byte) << shift;
             if ((byte >> 7) == 0)
                 break;
             shift += 7;
@@ -28,6 +30,7 @@ namespace leb
     }
 
     template<typename T>
+    inline
     T unsigned_decode(data& in)
     {
         T result = 0;
@@ -37,7 +40,7 @@ namespace leb
         {
             uint8_t byte = in.next();
 
-            result |= 0x7F << shift;
+            result |= ((0x7F & byte) << shift);
 
             if((byte >> 7) == 0)
                 break;
@@ -49,6 +52,32 @@ namespace leb
     }
 
     template<typename T>
+    inline
+    data signed_encode(T in)
+    {
+        data dtr;
+
+        bool more = 1;
+        bool negative = (in < 0);
+
+        while(more)
+        {
+            uint8_t byte = in & 0x7F;
+            in >>= 7;
+
+            if((in == 0 && ((byte & 0x40) == 0)) || (in == -1 && ((byte & 0x40) > 0)))
+                more = 0;
+            else
+                byte |= 0x80;
+
+            dtr.push_back(byte);
+        }
+
+        return dtr;
+    }
+
+    template<typename T>
+    inline
     data unsigned_encode(T in)
     {
         data dtr;
@@ -62,31 +91,6 @@ namespace leb
             dtr.push_back(byte);
         }
         while (in != 0);
-
-        return dtr;
-    }
-
-    template<typename T>
-    data signed_encode(T in)
-    {
-        data dtr;
-
-        bool more = 1;
-        bool negative = (in < 0);
-
-        while(more)
-        {
-            uint8_t byte = in & 0x7F;
-            in >>= 7;
-
-            /* sign bit of byte is second high order bit (0x40) */
-            if ((in == 0 && ((byte & 0x40) == 0)) || (in == -1 && ((byte & 0x40) == 1)))
-                more = 0;
-            else
-                byte |= 0x80;
-
-            dtr.push_back(byte);
-        }
 
         return dtr;
     }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 #include "LEB.hpp"
+#include <assert.h>
+#include <limits>
+
+void leb_tests()
+{
+    {
+        data d = leb::unsigned_encode<uint64_t>(80);
+
+        /*for(int i=0; i < d.size(); i++)
+        {
+            std::cout << std::to_string(d.ptr[i]) + "," << std::endl;
+        }*/
+
+        uint64_t original = leb::unsigned_decode<uint64_t>(d);
+
+        //std::cout << "orig " << original << std::endl;
+
+        assert(original == 80);
+    }
+
+    {
+        data dtr = leb::unsigned_encode<uint64_t>(0);
+
+        assert(leb::unsigned_decode<uint64_t>(dtr) == 0);
+    }
+
+    {
+        int64_t check = std::numeric_limits<int64_t>::min();
+
+        std::cout << "check " << check << std::endl;
+
+        data d = leb::signed_encode<int64_t>(check);
+
+        for(int i=0; i < d.size(); i++)
+        {
+            std::cout << std::to_string(d.ptr[i]) + "," << std::endl;
+        }
+
+        int64_t decoded = leb::signed_decode<int64_t>(d);
+
+        std::cout << "decoded " << decoded << std::endl;
+
+        assert(decoded == check);
+    }
+}
 
 int main()
 {
+    leb_tests();
 
     return 0;
 }
