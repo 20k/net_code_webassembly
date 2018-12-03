f9b4bb144bb5ac59e9adde79487f249ed83218fd
James <Icedshot@gmail.com>
2018-12-03 04:33:04 +0000

    beginnings of data storage

--- wasm_interop_context.hpp
+++ wasm_interop_context.hpp
@@ -3,10 +3,16 @@
 
 #include <variant>
 #include <nlohmann/json.hpp>
+#include <map>
+
+struct interop_element
+{
+    std::map<std::string, std::variant<nlohmann::json, void(*)(), std::shared_ptr<interop_element>>> data;
+};
 
 struct wasm_interop_context
 {
-
+    std::map<uint32_t, interop_element> elems;
 };
 
 #endif // WASM_INTEROP_CONTEXT_HPP_INCLUDED
